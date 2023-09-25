47d5f1f9ca3fa6f7e15a1e265221825047e45bee
JCKingNZ <jcking2468@gmail.com>
2023-09-25 18:59:12 +1300

    Added highscore system
    In this update I have added a highscore system but it is very buggy and needs some changes done to it

--- FlapTap/Assets/LogicScript.cs
+++ FlapTap/Assets/LogicScript.cs
@@ -8,15 +8,28 @@ public class LogicScript : MonoBehaviour
     //Defining different variables and objects
     public int playerScore;
     public Text scoreText;
+    public int highscore;
+    public Text highscoreText;
     public GameObject gameOverScreen;
 
+    void Start()
+    { 
+        //determines the current highscore and shows it upon game start.
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        highscoreText.text = "Highscore: " + highscore.ToString();
+    }
 
     //Counts player score
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
-        playerScore = playerScore + scoreToAdd;
+        //Counts player score
         scoreText.text = playerScore.ToString();
+        playerScore = playerScore + scoreToAdd;
+        //If the highscore is lower than the playscore this will update the highscore
+        if (highscore < playerScore)
+            PlayerPrefs.SetInt("highscore", playerScore);
+
 
     }
 
