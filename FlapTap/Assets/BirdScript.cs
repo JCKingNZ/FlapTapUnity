26483c2716e206470f781ac5510fb7bf33d472e6
JCKingNZ <jcking2468@gmail.com>
2023-10-17 13:06:44 +1300

    Tried making the bird flap
    it didnt work

--- FlapTap/Assets/BirdScript.cs
+++ FlapTap/Assets/BirdScript.cs
@@ -10,6 +10,8 @@ public class BirdScript : MonoBehaviour
     public LogicScript logic;
     public bool birdIsAlive = true;
     public GameObject pipeSpawner;
+    public Sprite wingsUp;
+    public Sprite wingsDown;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +19,52 @@ public class BirdScript : MonoBehaviour
         //Links the birdscript to the logic script
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         pipeSpawner.SetActive(false);
+
+        //StartCoroutine(waiter());
+
+        /*
+        for(int i = 0; i < iterationCount; i++)
+        {
+            invoke "changeSprite";
+        }*/
     }
+
+    /*
+    void changeSprite()
+    {
+        if (this.gameObject.GetComponent<SpriteRenderer>().sprite = wingsDown);
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = wingsUp;
+        }
+
+        else 
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = wingsDown;
+        }
+
+    }
+    */
+
+    /* IEnumerator waiter()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = wingsUp;
+
+        yield return new WaitForSeconds(0.1F);
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = wingsDown;
+    }
+    */
+
 
     // Update is called once per frame
     void Update()
-    {   //Takes the players input and moves the bird
+    {   
+        //Takes the players input and moves the bird
         if ( Input.GetKeyDown(KeyCode.Space) && birdIsAlive == true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
             pipeSpawner.SetActive(true);
+
         }
         //If the bird goes below y -12 than the game is over
         if (transform.position.y < -12 || transform.position.y > 13 )
